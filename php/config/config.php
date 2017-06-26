b22e4718d0cf384c196524e8e9a7230c362f2f1b
ultimo <von.ultimo@gmail.com>
2017-06-26 15:02:12 +0200

    config

--- php/config/config.php
+++ php/config/config.php
@@ -1,4 +1,5 @@
 <?php
-$db_user=kasse_user;
-$db_user_pw=K4b5xBnPrFGM4TRZ;
-$db=Kasse;
+$db_user = 'kasse_user';
+$db_user_pw = 'K4b5xBnPrFGM4TRZ';
+$db = 'Kasse';
+?>
--- php/db_abfragen.php
+++ php/db_abfragen.php
@@ -1,15 +1,12 @@
 <?php
 $mysqli;
-
 function connect() {
     global $mysqli;
     require 'config/config.php';
     if( isset( $mysqli ) ) {
         return;
     }
-
     $mysqli = new mysqli("localhost", $db_user, $db_user_pw, $db);
-
     $mysqli->query("SET NAMES 'utf8'");
     if ($mysqli->connect_errno) {
         echo "MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
@@ -18,21 +15,15 @@ function connect() {
     //echo $mysqli->host_info . "<br>\n";
     return $mysqli;
 }
-
 function getUser(){
     global $mysqli;
     $sql = "SELECT * FROM user";
     connect();
     $erg=  $mysqli->query($sql);
     print_r($erg);
-
 }
-
 function getBuchung(){
-
 }
-
 function getKontostand($user){
-
 }
 ?>
