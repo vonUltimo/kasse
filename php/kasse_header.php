d1917a0fd2b5a94caced4df5d4cd442631b9a9c1
jpnem <jpnemeth3012@googlemail.com>
2017-10-18 19:57:42 +0200

    update

--- php/kasse_header.php
+++ php/kasse_header.php
@@ -4,50 +4,52 @@
  * @author Jan Nemeth <jpnemeth3012@gmail.com>
  * Version: 0.1
  */
-function kasse_header(){
+function kasse_header()
+{
     echo "
     <div class=\"container\">
     <header>
         <img class=\"header-img\" src=\"/kasse/pics/logo.png\"><br/>
-        <div>
+        <div>";
+            if ($_SESSION['usergroup'] < 5) {echo "
             <div class=\"dropdown\">
                 <button class=\"button-primary\">Ausgaben</button>
                 <div class=\"dropdown-content\">";
-                if ($_SESSION['usergroup'] < 2){ echo "
-                    <a href='get_booking.php'>Buchung anzeigen</a>
-                    <a href=\"get_konto.php\">Kontostand anzeigen</a>
-                    ";};
-                echo "
-                </div>
-            </div>
+                    if ($_SESSION['usergroup'] < 5) {echo "<a href='get_booking.php'>Buchung anzeigen</a>";}
+                    if ($_SESSION['usergroup'] < 5) {echo "<a href=\"get_konto.php\">Kontostand anzeigen</a>";}
+                echo "</div>
+            </div>";}
+            if ($_SESSION['usergroup'] < 3) {echo "
             <div class=\"dropdown\">
                 <button class=\"button-primary\">Eingaben</button>
-                <div class=\"dropdown-content\">
-                    <a href=\"add_booking.php\">Buchung anlegen</a>
-                    <a href=\"correctBooking.php\">letzte Buchung korrigieren unfertig</a>
-                </div>
-            </div>
+                <div class=\"dropdown-content\">";
+                    if ($_SESSION['usergroup'] < 3) {echo "<a href=\"add_booking.php\">Buchung anlegen</a>";}
+                    if ($_SESSION['usergroup'] < 3) {echo "<a href=\"correctBooking.php\">letzte Buchung korrigieren unfertig</a>";}
+                echo"</div>
+            </div> ";}
+            if ($_SESSION['usergroup'] < 3) {echo "
             <div class=\"dropdown\">
                 <button class=\"button-primary\">Nutzerverwaltung</button>
-                <div class=\"dropdown-content\">
-                    <a href=\"memberlist.php\">Mitgliederliste anzeigen</a>
-                    <a href=\"addUser.php\">Nutzer hinzufügen</a>
-                    <a href=\"updateUser.php\">Nutzer aktualisieren</a>     
-                </div>
-            </div>
+                <div class=\"dropdown-content\">";
+                    if ($_SESSION['usergroup'] < 3) {echo "<a href=\"memberlist.php\">Mitgliederliste anzeigen</a>";}
+                    if ($_SESSION['usergroup'] < 3) {echo "<a href=\"addUser.php\">Nutzer hinzufügen</a>";}
+                    if ($_SESSION['usergroup'] < 3) {echo "<a href=\"updateUser.php\">Nutzer aktualisieren</a>";}
+                echo"</div>
+            </div>";}
+            if ($_SESSION['usergroup'] < 3) {echo "
             <div class=\"dropdown\">
                 <button class=\"button-primary\">Datenbankverwaltung</button>
-                <div class=\"dropdown-content\">
-                    <a href=\"addPurpose.php\">Verwendungszweck hinzufügen</a>
-                    <a href=\"addClub.php\">Verein hinzufügen</a>
-                    <a href=\"#\">Buchung löschen unfertig</a>
-                </div>
-            </div>
+                <div class=\"dropdown-content\">";
+                    if ($_SESSION['usergroup'] < 5) {echo "<a href=\"addPurpose.php\">Verwendungszweck hinzufügen</a>";}
+                    if ($_SESSION['usergroup'] < 5) {echo "<a href=\"addClub.php\">Verein hinzufügen</a>";}
+                    if ($_SESSION['usergroup'] < 5) {echo "<a href=\"#\">Buchung löschen unfertig</a>";}
+                    echo"</div>
+                </div>";}
+            if ($_SESSION['usergroup'] < 100) {echo "
             <div class=\"dropdown\">
-                <a href=\"/kasse/php/logout.php\">
-                    <button class=\"button-primary\" id=\"logout\"><strong>Logout</strong></button>
-                </a>
-            </div>
+                <a href=\"/kasse/php/logout.php\"><button class=\"button-primary\" id=\"logout\"><strong>Logout</strong></button></a>
+            </div>";}
+echo"            
         </div>
     </header>
 </div>";}
